a5ae94a817de068a28fbce1ce500168bac941dd9
perkles <testdota@hotmail.com>
2022-06-14 00:29:26 -0300

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -10,3 +10,29 @@ function decrement() {
   currentNumber = currentNumber - 1;
   IDcurrentNumber.innerHTML = currentNumber;
 }
+
+function adjustcolors() {
+  if (currentNumber > 5) {
+    IDcurrentNumber.style.color = "Blue";
+  } else {
+    IDcurrentNumber.style.color = "Black";
+  }
+  if (currentNumber < -0) {
+    IDcurrentNumber.style.color = "Red";
+  } else {
+    IDcurrentNumber.style.color = "Black";
+  }
+}
+
+function stopcontador() {
+  if (currentNumber >= 10) {
+    document.getElementById("adicionar").disabled = true;
+  } else {
+    document.getElementById("adicionar").disabled = false;
+  }
+  if (currentNumber <= -10) {
+    document.getElementById("subtrair").disabled=true;
+  } else {
+    document.getElementById("subtrair").disabled = false;
+  }
+}
